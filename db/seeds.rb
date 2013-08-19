91abc1566639b0880d639cdc98d2f382eedf7531
Yuzhe <yuzhe.c85@gmail.com>
2013-08-19 13:44:45 +0800

    first deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.new
+admin.email = "yuzhe@summers.com.tw"
+admin.password = "11111111"
+admin.password_confirmation = "11111111"
+admin.encrypted_password = "$2a$10$TfsZ3nHA6LHzuleQC3PeMua.Waj2dD2qtLrvhDX1bmkV35TVBq/96"
+admin.save
